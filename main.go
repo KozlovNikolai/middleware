6103c055e0c58a55ea033f4cdc099be8bb8def34
Nikolay Kozlov <gremiha3@gmail.com>
2024-03-30 20:31:01 +0300

    Запустил сервер с ответом 200 и словом - тест. Всё в одном файле

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
+	s := echo.New()
+	s.GET("/status", handler)
+	err := s.Start(":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println("server running. ")
+
+}
+
+func handler(ctx echo.Context) error {
+	err := ctx.String(http.StatusOK, "test")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return nil
 }
